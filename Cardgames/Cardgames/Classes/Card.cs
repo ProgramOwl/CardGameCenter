5a4926506061fe598e5f98eeb8a21a1ba505f1c1
Unknown <CHawley@student.neumont.edu>
2018-01-26 18:31:13 -0700

    Added card class, suit enum

---
+++ Cardgames/Cardgames/Classes/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cardgames.Classes
+{
+    public class Card
+    {
+        private CardSuits suit;
+
+        public CardSuits Suit
+        {
+            get { return suit; }
+            set { suit = value; }
+        }
+
+    }
+}

d7da8bc7ccd021c4c09890d546e3ce29cadd0559
Ginger John <JAuchterlonie@student.neumont.edu>
2018-01-26 19:22:20 -0700

    more Jack black

--- Cardgames/Cardgames/Blackjack.xaml.cs
+++ Cardgames/Cardgames/Blackjack.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Cardgames.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,60 @@ namespace Cardgames
         public Blackjack()
         {
             InitializeComponent();
+        }
+        public void StartGame(int players)
+        {
+            BlackJackDealer dealer = new BlackJackDealer(0, 0);
+            if(players == 5)
+            {
+                Player5Game();
+            } else if(players == 4)
+            {
+                Player4Game();
+            } else if(players == 3)
+            {
+                Player3Game();
+            } else if(players == 2)
+            {
+                Player2Game();
+            } else
+            {
+                Player1Game();
+            }
+        }
+        private void Player1Game()
+        {
+            Player player1 = new Player("Player 1", 0, 20, 0, 0);
+        }
+
+        private void Player2Game()
+        {
+            Player player1 = new Player("Player 1", 0, 20, 0, 0);
+            Player player2 = new Player("Player 2", 0, 20, 0, 0);
+        }
+
+        private void Player3Game()
+        {
+            Player player1 = new Player("Player 1", 0, 20, 0, 0);
+            Player player2 = new Player("Player 2", 0, 20, 0, 0);
+            Player player3 = new Player("Player 3", 0, 20, 0, 0);
+        }
+
+        private void Player4Game()
+        {
+            Player player1 = new Player("Player 1", 0, 20, 0, 0);
+            Player player2 = new Player("Player 2", 0, 20, 0, 0);
+            Player player3 = new Player("Player 3", 0, 20, 0, 0);
+            Player player4 = new Player("Player 4", 0, 20, 0, 0);
+        }
+
+        private void Player5Game()
+        {
+            Player player1 = new Player("Player 1", 0, 20, 0, 0);
+            Player player2 = new Player("Player 2", 0, 20, 0, 0);
+            Player player3 = new Player("Player 3", 0, 20, 0, 0);
+            Player player4 = new Player("Player 4", 0, 20, 0, 0);
+            Player player5 = new Player("Player 5", 0, 20, 0, 0);
         }
     }
 }
--- Cardgames/Cardgames/Classes/BlackJackDealer.cs
+++ Cardgames/Cardgames/Classes/BlackJackDealer.cs
@@ -8,10 +8,12 @@ namespace Cardgames
 {
     public class BlackJackDealer
     {
+        public string Name { get; set; }
         public int DealerHand { get; set; }
         public int DealerScore { get; set; }
         public BlackJackDealer(int hand, int dScore)
         {
+            Name = "Dealer";
             DealerHand = hand;
             DealerScore = dScore;
         }

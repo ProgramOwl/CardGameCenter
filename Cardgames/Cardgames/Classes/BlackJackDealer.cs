ab8bc4af964162833a74ce1de9efa9e754fddcd3
Ginger John <JAuchterlonie@student.neumont.edu>
2018-01-26 19:01:35 -0700

    black Jacks' logic

---
+++ Cardgames/Cardgames/Classes/BlackJackDealer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cardgames
+{
+    public class BlackJackDealer
+    {
+        public int DealerHand { get; set; }
+        public int DealerScore { get; set; }
+        public BlackJackDealer(int hand, int dScore)
+        {
+            DealerHand = hand;
+            DealerScore = dScore;
+        }
+    }
+}
---
+++ Cardgames/Cardgames/Classes/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cardgames
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int PlayerHand { get; set; }
+        public int PlayerBank { get; set; }
+        public int GoFishCounter { get; set; }
+        public int BlackjackScore { get; set; }
+        public Player(string name, int hand, int bank, int fishScore, int bljScore)
+        {
+            Name = name;
+            PlayerHand = hand;
+            PlayerBank = bank;
+            GoFishCounter = fishScore;
+            BlackjackScore = bljScore;
+        }
+    }
+}

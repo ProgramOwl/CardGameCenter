d4e63b95e5cdd62a104db4362e44243141904f64
ProgramOwl <swaugh@student.neumont.edu>
2018-01-28 18:30:14 -0800

    Made the home layout and applied a bit of control code in MainWindow.xaml.cs, and added the Images file and current background imges.

--- Cardgames/Cardgames/MainWindow.xaml.cs
+++ Cardgames/Cardgames/MainWindow.xaml.cs
@@ -24,5 +24,44 @@ namespace Cardgames
         {
             InitializeComponent();
         }
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            //if (TimesClosed == 0)
+            //{
+            //    MessageBox.Show("You haven't saved recently, are you sure you want to close it?");
+            //    TimesClosed = 1;
+            //}
+            //else
+            //{
+                //MessageBox.Show("Ok, I'll really close this time.");
+                Application.Current.Shutdown();
+            //}
+        }
+
+        private void GoFishButton_Click(object sender, RoutedEventArgs e)
+        {
+            HomeTextPage.Visibility = Visibility.Collapsed;
+            BlackJackForm.Visibility = Visibility.Collapsed;
+            GoFishForm.Visibility = Visibility.Visible;
+        }
+
+        private void BlackJackButton_Click(object sender, RoutedEventArgs e)
+        {
+            HomeTextPage.Visibility = Visibility.Collapsed;
+            GoFishForm.Visibility = Visibility.Collapsed;
+            BlackJackForm.Visibility = Visibility.Visible;
+        }
+
+        private void StartGoFishButton_Click(object sender, RoutedEventArgs e)
+        {
+            int number = AmountOfPlayersGo.SelectedIndex;
+            //call to page
+        }
+
+        private void StartBlackJackButton_Click(object sender, RoutedEventArgs e)
+        {
+            int number = AmountOfPlayersBlack.SelectedIndex;
+            //call to page
+        }
     }
 }

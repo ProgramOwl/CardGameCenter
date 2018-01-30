ef0c47b3a30f133cb368b086ad59aad5fb07b8ca
Unknown <CHawley@student.neumont.edu>
2018-01-29 18:26:13 -0700

    Deck now has cards in it

--- Cardgames/Cardgames/Classes/Deck.cs
+++ Cardgames/Cardgames/Classes/Deck.cs
@@ -17,17 +17,24 @@ namespace Cardgames.Classes
         }
         public void FillDeck()
         {
+            cards = new Card[52];
             CardValue[] ranks = (CardValue[])Enum.GetValues(typeof(CardValue));
             CardSuit[] suits = (CardSuit[])Enum.GetValues(typeof(CardSuit));
             Card tempCard;
+            int cardNumber = 0;
             for(int i = 0; i < suits.Length; i++)
             {
                 for(int j = 0; j < ranks.Length; j++)
                 {
                     tempCard = new Card(suits[i], ranks[j]);
-                    
+                    cards[cardNumber] = tempCard;
+                    cardNumber++;
                 }
             }
+        }
+        public Deck()
+        {
+            FillDeck();
         }
     }
 }
--- Cardgames/Cardgames/MainWindow.xaml.cs
+++ Cardgames/Cardgames/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Cardgames.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace Cardgames
         public MainWindow()
         {
             InitializeComponent();
+            DeckFillTest();
         }
         private void Close_Click(object sender, RoutedEventArgs e)
         {
@@ -62,6 +64,10 @@ namespace Cardgames
         {
             int number = AmountOfPlayersBlack.SelectedIndex;
             //call to page
+        }
+        public void DeckFillTest()
+        {
+            Deck tempDeck = new Deck();
         }
     }
 }

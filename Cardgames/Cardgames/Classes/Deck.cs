c20fd14bc17f626c3be536c617909c26ba8b70f8
Unknown <CHawley@student.neumont.edu>
2018-01-29 17:59:57 -0700

    just pullin

---
+++ Cardgames/Cardgames/Classes/Deck.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cardgames.Classes
+{
+    public class Deck
+    {
+        private Card[] cards;
+
+        public Card[] Cards
+        {
+            get { return cards; }
+            set { cards = value; }
+        }
+        public void FillDeck()
+        {
+            CardValue[] ranks = (CardValue[])Enum.GetValues(typeof(CardValue));
+            CardSuit[] suits = (CardSuit[])Enum.GetValues(typeof(CardSuit));
+            Card tempCard;
+            for(int i = 0; i < suits.Length; i++)
+            {
+                for(int j = 0; j < ranks.Length; j++)
+                {
+                    tempCard = new Card(suits[i], ranks[j]);
+                    
+                }
+            }
+        }
+    }
+}
